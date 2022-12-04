400b680b7cfb7bde20b220b001bf5af36736d5ba
latifatuzikra-suhairi <latifatuzikra@gmail.com>
2022-12-05 01:35:32 +0700

    adding web cam

--- js/webcam.js
+++ js/webcam.js
@@ -1,62 +1,20 @@
-var infourl = new URLSearchParams(window.location.search);
-			
-			var devicesaya = [];
-			var devicesaatini = 0;
-			
-			var fm = "user";
-			
-			if(infourl.get("modekamera") == "belakang"){
-				fm = { "exact" : "environment" };
-			}
-			
-			var wVideo = document.querySelector("#webcamVideo");
-			var wCanvas = document.querySelector("#webcamCanvas");
-			
-			//list device-device video
-			navigator.mediaDevices.enumerateDevices().then(function(devices){
-				devices.forEach(function(device){
-					if(device.kind == "videoinput"){
-						devicesaya.push({
-							"id" : device.deviceId,
-							"label" : device.label,
-						});
-					}
-				});
-			});
-			
-			setTimeout(function(){
-				$("#dsi").html(devicesaya[devicesaatini].label);
-			},1000);
-			
-			
-			async function startCamera(){
-				var stream = null;
-				try{
-					
-					stream = await navigator.mediaDevices.getUserMedia({ video : { 
-						deviceId : devicesaya[devicesaatini].id, 
-						facingMode : fm ,
-					}, audio : false });
-					
-					
-				}catch(error){
-					//console.log(error);
-					//alert("Perangkat ini tidak dilengkapi kamera belakang.");
-				}
-				
-				wVideo.srcObject = stream;
-			}
-			
-			function ambilGambar(){
-				wCanvas.getContext("2d").drawImage(wVideo, 0, 0, 720, 480);
-				var imageData = wCanvas.toDataURL("image/jpg");
-				console.log(imageData);
-				
-				toggleCanvasVideo();
-			}
-			
-			function toggleCanvasVideo(){
-				$("#webcamVideo").toggle();
-				$("#webcamCanvas").toggle();
-			}
-		
+
+function showCamera(){
+    Webcam.set({
+        width: 400,
+        height: 300,
+        image_format: 'jpeg',
+        jpeg_quality: 100
+    });
+    Webcam.attach('#my_camera');
+}
+
+function takeSnapshot() {
+        // take snapshot and get image data
+        Webcam.snap( function(data_uri) {
+            // display results in page
+            document.getElementById('cam_results').innerHTML = 
+                '<p class="text-primary mb-6">Hasil foto: </p>' +
+                '<img src="'+data_uri+'"/>';
+        } );
+    }
